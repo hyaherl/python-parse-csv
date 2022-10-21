20b9dae18ab34e17d7d75536a8d0829958262c76
DH <hyaherl@gmail.com>
2022-10-21 18:29:32 +0900

    change

--- parseCsvToJson.py
+++ parseCsvToJson.py
@@ -3,8 +3,14 @@ import pandas as pd
 input_dir = "./input"
 output_dir = "./output"
 
-data = pd.read_csv(file_path)
+file_path = input_dir + '/raw_test.csv'
 
-with open('output_file_path', 'w', encoding='utf-8') as f:
-    data.to_json(f, force_ascii=False, orient='records')
+df = pd.read_csv(file_path)
+
+print(df.info())
+print(df.describe())
+
+# json 파일 저장
+# with open('output_file_path', 'w', encoding='utf-8') as f:
+#     data.to_json(f, force_ascii=False, orient='records')
     
